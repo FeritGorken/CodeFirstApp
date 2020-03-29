c87ef0554f197d8f1240977d60c650f8fae35fda
Ferit GÖRKEN <feritgorken1@gmail.com>
2020-03-29 15:11:24 +0300

    Add project files.

---
+++ CodeFirstApp/Entity/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstApp.Entity
+{
+    public class Customer
+    {
+        //ilk orderin null gelme olayını yok etmek için constructor yaptım
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+        public string CustomerID { get; set; }
+        public string ContactName { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        //Bir müşterinin birden fazla müşterisi olabilir durumu
+        public List<Order> Orders { get; set; }
+    }
+}
---
+++ CodeFirstApp/Entity/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstApp.Entity
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipCountry { get; set; }
+        public DateTime OrderDate { get; set; }
+        //Bir siparişin bir müşterisi vardır
+        public Customer Customer { get; set; }
+
+    }
+}
